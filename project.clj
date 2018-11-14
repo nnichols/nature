4e70894c13c2d5f99dd21dc90c800af1edaba42f
Nick A. Nichols <nichols1991@gmail.com>
2018-11-13 21:30:14 -0600

    Clean-up project.clj, add GH url

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject nature "0.1.0-SNAPSHOT"
   :description "A simple genetic algorithms library for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/nnichols/nature"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
-                 [danlentz/clj-uuid   "0.1.7"]
                  [com.taoensso/tufte "2.0.1"]
                  [com.clojure-goes-fast/clj-memory-meter "0.1.2"]]
   :plugins [[lein-codox "0.10.5"]]
