72ae1d83dd262a77f7ed6134e10967e51a5d332b
Nick A. Nichols <nichols1991@gmail.com>
2018-11-13 21:39:43 -0600

    Creeate dummy constructor

--- src/nature/core.clj
+++ src/nature/core.clj
@@ -6,11 +6,13 @@
   []
   (str (java.util.UUID/randomUUID)))
 
-(def data-model
-  {:genetic-sequence [0 1 0 1 1 1]
-   :guid (uuid)
-   :age 1
-   :fitness-score 0.1M})
+(defn build-individual
+  "Create a hard-coded individual"
+  []
+  (assoc {} :genetic-sequence [0 1 0 1 1 1]
+         :guid (uuid)
+         :age 1
+         :fitness-score 0.1M))
 
 (s/def ::genetic-sequence
   (s/and vector?
@@ -31,4 +33,4 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (s/valid? ::individual data-model)))
+  (println (s/valid? ::individual (build-individual))))
