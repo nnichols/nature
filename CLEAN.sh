1867f1d8ce307ce03fed48830681a6bd67f24201
Nick A. Nichols <nichols1991@gmail.com>
2018-11-17 09:08:38 -0600

    Added api docs and coverage reports

--- CLEAN.sh
+++ CLEAN.sh
@@ -1,1 +1,24 @@
-lein test
+#!/usr/bin/env bash
+
+## Correct formatting
+lein cljfmt fix
+
+echo " "
+echo "Bikeshed Style Analysis"
+lein bikeshed -v
+
+echo " "
+echo "Eastwood Linting"
+lein eastwood
+
+echo " "
+echo "Eastwood Linting"
+lein cloverage
+
+echo " "
+echo "Unit Tests & Coverage Reporting"
+lein cloverage -o docs/coverage
+
+echo " "
+echo "API Reporting"
+lein codox
--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to nature
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
