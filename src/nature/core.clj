de09965e9921caf23ad5f263b1a31330dd28257f
Nick A. Nichols <nichols1991@gmail.com>
2018-11-17 09:30:01 -0600

    Build populations of individuals

--- src/nature/core.clj
+++ src/nature/core.clj
@@ -6,17 +6,25 @@
   []
   (str (java.util.UUID/randomUUID)))
 
+(defn generate-sequence
+  [allele-set sequence-length]
+  (repeatedly sequence-length #(rand-nth allele-set)))
+
 (defn build-individual
   "Create a generated individual"
   [allele-set sequence-length fitness-function]
-  (let [genes (repeatedly sequence-length #(rand-nth allele-set))]
+  (let [genes (generate-sequence allele-set sequence-length)]
     (assoc {} :genetic-sequence genes
            :guid (uuid)
            :parents ["Initializer"]
            :age 0
            :fitness-score (fitness-function genes))))
 
+(defn build-population
+  [population-size allele-set sequence-length fitness-function]
+  (repeatedly population-size #(build-individual allele-set sequence-length fitness-function)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (build-individual [1 0] 5 (partial apply +))))
+  (println (build-population 5 [1 0] 5 (partial apply +))))
