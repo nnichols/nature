8ad11bf8d90808510d87b2424c2c92bc850c00cb
nnichols <nichols1991@gmail.com>
2022-05-25 15:02:56 +0000

    Bump org.clojure/clojure from 1.10.3 to 1.11.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
             :url "https://github.com/nnichols/nature"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.10.3"]
+            :dependencies [[org.clojure/clojure "1.11.1"]
                            [org.clojure/clojurescript "1.10.844" :scope "provided"]
                            [cljx-sampling "0.1.0"]]
 
