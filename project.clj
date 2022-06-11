1170296199363f6e747b70729279652037b66213
nnichols <nichols1991@gmail.com>
2022-06-11 15:02:40 +0000

    Bump org.clojure/clojurescript from 1.10.844 to 1.11.57

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.11.1"]
-                           [org.clojure/clojurescript "1.10.844" :scope "provided"]
+                           [org.clojure/clojurescript "1.11.57" :scope "provided"]
                            [cljx-sampling "0.1.0"]]
 
             :plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]
